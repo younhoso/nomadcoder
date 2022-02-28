7489b2da05f33488828b71e4187ec090bc78d661
younhoso <thdbsgh3443@naver.com>
2022-02-28 16:15:44 +0900

    To Do List part One

---
+++ moviepedia/create-react-app/todo-list/src/App.js
@@ -0,0 +1,27 @@
+import {useState} from "react";
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (e) => setToDo(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if(toDo === ""){
+      return
+    }
+    setToDos(currentArray => [toDo, ...currentArray]);
+    setToDo("");
+  }
+
+  return (
+    <div>
+      <h1>My To Dos({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do...."/>
+        <button>Add To Do</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
