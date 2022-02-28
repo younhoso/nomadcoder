2cf4ae39a80219bd3b3ab8590074c9e70947ae00
younhoso <thdbsgh3443@naver.com>
2022-02-28 15:23:53 +0900

    Cleanup useEffect

--- moviepedia/create-react-app/react-for-beginners/src/App.js
+++ moviepedia/create-react-app/react-for-beginners/src/App.js
@@ -2,33 +2,28 @@ import Button from "./Button";
 import styles from "./App.module.css"
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-
-  const iRunOnlyOnce = () => setValue((prev) => prev + 1);
-  const onChange = (e) => setKeyword(e.target.value)
+function Hello() {
   useEffect(() => {
-    console.log('나는 최초에 한번만 실행돼요')
-  }, [])
+    console.log("created :)");
+    return function() {
+      console.log("bye :(");
+    }
+  }, []);
 
-  useEffect(() => { // 키워드 state가 변할때 실행하고 싶은 시점
-    console.log('나는 키워드가 변화할 때 실행돼요')
-  }, [keyword])
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  },[])
+  return <h1>Hello</h1>
+}
 
-  useEffect(() => { // 카운터 state가 변할때 실행하고 싶은 시점
-    console.log('나는 카운터가 변화할 때 실행돼요')
-  }, [counter])
-
-  useEffect(() => { // 원한다면 두개의 state 값이 변할때 실행하고 싶은 시점(두 개의state를 합칠수도 있다.)
-    console.log('나는 키워크와 카운터가 변화할 때 실행돼요!!')
-  }, [keyword, counter])
-
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div className="App">
-      <input onChange={onChange} value={keyword} placeholder="Search here..." />
-      <h1 className={styles.title}>{counter}</h1>
-      <Button onClick={iRunOnlyOnce}>counter!!!</Button>
+      {showing ? <Hello /> : null}
+      <Button onClick={onClick}>{showing ? "Hide" : "Show"}</Button>
     </div>
   );
 }
