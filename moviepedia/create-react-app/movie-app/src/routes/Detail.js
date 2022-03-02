e6c3efd68ddb6e9973dd17276df9c8151192432e
younhoso <thdbsgh3443@naver.com>
2022-03-02 17:14:45 +0900

    Router parameters 설정 및 상세페이지 전환 설정

--- moviepedia/create-react-app/movie-app/src/routes/Detail.js
+++ moviepedia/create-react-app/movie-app/src/routes/Detail.js
@@ -1,4 +1,31 @@
+import { useEffect, useState } from 'react';
+import {useParams} from 'react-router-dom'
+
 function Detail() {
-	return <div>Detail</div>;
+	const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+	const {id} = useParams();
+	const getMovie = async () => {
+		const data = await(await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json();
+		setMovies(data['data']['movie'])
+		setLoading(false);
+	};
+	useEffect(() => {
+		getMovie()
+	},[])
+	const {title, rating, medium_cover_image, description_intro} = movies;
+	return(
+		<div>
+			{loading ? (
+				<h1>Loading...</h1>
+			) : (
+				<div>
+					 <h1>{title} {rating}</h1>
+					 <img src={medium_cover_image}></img>
+					 <p>{description_intro}</p>
+				</div>
+			)}
+		</div>
+	);
 }
 export default Detail;
--- moviepedia/create-react-app/movie-app/src/routes/Home.js
+++ moviepedia/create-react-app/movie-app/src/routes/Home.js
@@ -13,7 +13,7 @@ function Home() {
     getMovies()
   }, [])
   return (
-    <div>
+    <div> 
       {loading ? (
         <h1>Loading...</h1>
       ) : (
@@ -21,6 +21,7 @@ function Home() {
           {movies.map((movie) => (
             <Movie 
               key={movie.id}
+              id={movie.id}
               coverImg={movie.medium_cover_image}
               title={movie.title}
               summary={movie.summary}
