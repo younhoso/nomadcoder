4b03a7ad3de85b30b9081425b988241678096a15
younhoso <thdbsgh3443@naver.com>
2022-03-03 21:10:31 +0900

    Styles 추가

--- moviepedia/create-react-app/movie-app/src/routes/Home.js
+++ moviepedia/create-react-app/movie-app/src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import Movie from "../components/Movie"
+import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 function Home() {
   const [loading, setLoading] = useState(true);
@@ -13,11 +14,11 @@ function Home() {
     getMovies()
   }, [])
   return (
-    <div> 
+    <div className={styles.container}>
       {loading ? (
         <h1>Loading.....</h1>
       ) : (
-        <div>
+        <div className={styles.movies}>
           {movies.map((movie) => (
             <Movie 
               key={movie.id}
