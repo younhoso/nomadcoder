05548eb8592a5d8af49e9f558bb6d176428fd83a
younhoso <thdbsgh3443@naver.com>
2022-03-30 17:33:03 +0900

    2.2 Adapting and Extending

--- react-master-class/src/App.js
+++ react-master-class/src/App.js
@@ -4,28 +4,21 @@ const Father = styled.div`
   display: flex
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
-`;
-const BoxTwo = styled.div`
-  background-color: tomato;
+const Box = styled.div`
+  background-color: ${(props => props.bgColor)};
   width: 100px;
   height: 100px;
 `;
 
-const Text = styled.h1`
-  color: white;
+const Circle = styled(Box)`
+  border-radius: 50px;
 `;
 
 function App() {
   return (
-    <Father>{/* */}
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+    <Father>
+      <Box bgColor="teal"/>
+      <Circle bgColor="#333"/>
     </Father>
   );
 }
