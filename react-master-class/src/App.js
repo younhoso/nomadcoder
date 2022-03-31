faea53a084b936c071765b4906edcb32ccafea34
younhoso <thdbsgh3443@naver.com>
2022-03-31 17:11:21 +0900

    'As' and Attrs

--- react-master-class/src/App.js
+++ react-master-class/src/App.js
@@ -4,21 +4,18 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: ${(props) => props.width}px;
-  height: 100px;
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50px;
+const Input = styled.input.attrs({ required: true })`
+  background-color: tomato;
 `;
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal" width="100" />
-      <Circle bgColor="#333" width="100" />
+    <Father as="header">
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
