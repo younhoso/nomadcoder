9b9972fe44630bc74b6a2812d44136953803bd00
younhoso <thdbsgh3443@naver.com>
2022-04-05 15:31:06 +0900

    3번째 섹션 복습

--- react-master-class/src/App.tsx
+++ react-master-class/src/App.tsx
@@ -1,29 +1,33 @@
-import React, { useState } from "react";
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+interface DummyProps {
+  text: string;
+  active?: boolean;
+}
+
+function Dummy({ text, active = false }: DummyProps) {
+  return <H1>{text}</H1>;
+}
 
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = e;
-    setValue(value);
-  };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("hello", value);
+  const onClick = (event: React.FormEvent<HTMLButtonElement>) => {
+    console.log(event);
   };
   return (
-    <div>
-      <form action="" onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>protected</H1>
+      <Dummy active={true} text="hello" />
+      <button onClick={onClick}>click me</button>
+    </Container>
   );
 }
 
